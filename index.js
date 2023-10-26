1ee9e3fa3bd17ba9545a43f31d7cd12aaa6b2c73
gabrielabalieiro <gabibalieirosantos@gmail.com>
2023-10-26 15:49:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { application } = require("express")
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname, "templates")
+
+app.get('/', (requisicao, resposta) =>{
+    resposta.sendFile(``)
+})
+
+app.listen(3000, () => {
+    console.log("Sevidor rodando na porta 3000!")
+})
